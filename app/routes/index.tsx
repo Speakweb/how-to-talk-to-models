b20d2ef2b36fb640c199f4e5cfd3ae898e4db3fb
Anton-Pan <anton.v.panfilenok@gmail.com>
2023-08-30 18:02:49 -0700

    turned index.tsx into a chatgpt interaction game based on specifications provided in issue #1 TODO:     - add syntax hgihlighting for left and right panels     - figure out a way to check if the response code from chatgpt (using the user's instructions) is correct in respect to on the challenge given by the game

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useCallback, useState} from 'react';
+import React, {useRef, useCallback, useState, useEffect} from 'react';
 import {Configuration, OpenAIApi, ChatCompletionRequestMessage} from 'openai';
 import {type ActionArgs} from '@remix-run/node';
 import {Form, Link, useActionData, useNavigation, useSubmit} from '@remix-run/react';
@@ -18,9 +18,10 @@ export interface ChatHistoryProps extends ChatCompletionRequestMessage {
 
 export async function action({request}: ActionArgs): Promise<ReturnedDataProps> {
   const body = await request.formData();
-  const message = body.get('message') as string;
+  let message = body.get('message') as string;
+  const exampleCode = `console.log("Hello World!")`
   const chatHistory = JSON.parse(body.get('chat-history') as string) || [];
-
+  message = `Modify the following code:\n` + exampleCode + `\nTo these specifications: \n` + message + `\n Respond using ONLY executable code, with nothing else in your reply.`;
   const conf = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
@@ -65,18 +66,31 @@ export default function IndexPage() {
   const submit = useSubmit();
   const [chatHistory, setChatHistory] = useState<ChatHistoryProps[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [randomCodeString, setRandomCodeAndChallengeString] = useState<string>('');
+  const [gptResponse, setGptResponse] = useState<string>(''); // Added state for GPT response
 
   const isSubmitting = navigation.state === 'submitting';
 
   const saveUserMessage = (message: string) => {
     setChatHistory(prevChatHistory => [...prevChatHistory, {role: 'user', content: message}]);
   };
-  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => { // Made the function async
     const formData = new FormData(event.target as HTMLFormElement);
     const message = formData.get('message');
 
     saveUserMessage(message as string);
+    
+    submit(formData);
+    // Call the action function with the message
   };
+
+  useEffect(() => {
+    if (data) {
+      setGptResponse(data.answer);
+      // alert("Congratulations!\n You Won!");
+    }
+  }, [data]);
 
   const submitFormOnEnter = useCallback((event: React.KeyboardEvent<HTMLTextAreaElement>) => {
     const value = (event.target as HTMLTextAreaElement).value;
@@ -84,8 +98,22 @@ export default function IndexPage() {
     if (event.key === 'Enter' && !event.shiftKey && value.trim().length > 2) {
       saveUserMessage(value);
       submit(formRef.current, {replace: true});
+      inputRef.current!.value = ''; // Clear the text inside the text box
     }
   }, [submit, formRef, saveUserMessage]);
+
+  useEffect(() => {
+    const startingCodeArray = [
+      `console.log("Hello, World!");` // JavaScript code
+    ];
+    const startingChallengeArray = [
+      `Modify the following code to print out "Hi!" five times. --> \n`
+    ]
+    // const randomIndex1 = Math.floor(Math.random() * startingChallengeArray.length);
+    // const randomIndex2 = Math.floor(Math.random() * startingCodeArray.length);
+
+    setRandomCodeAndChallengeString(startingChallengeArray[0] + startingCodeArray[0]);
+  }, []);
 
   if (error) {
     return (
@@ -104,39 +132,45 @@ export default function IndexPage() {
   }
 
   return (
-    <main className="container mx-auto rounded-lg h-full grid grid-cols-3 gap-4 p-4 pb-0 sm:p-8 sm:pb-0 max-w-full sm:max-w-auto">
-      <div className="box-container p-4 sm:p-8 backdrop-blur-md">
-        <h2 className="header">Unedited Code</h2>
-        <div className="content-box">
-          {/* Unedited code goes here */}
+    <main className="container mx-auto rounded-lg h-full grid grid-cols-3 gap-4 p-4 pb-0 sm:p-8 sm:pb-0 max-w-full sm:max-w-auto w-full">
+      <div className="box-container p-4 sm:p-8 backdrop-blur-md border border-black">
+        <h2 className="header font-bold text-lg">Code Display</h2> {/* Made the header text bold and slightly bigger */}
+        <div className="content-box" style={{ wordWrap: 'break-word', overflow: 'auto'}}> {/* Added wordWrap and overflow styles to prevent horizontal overflow */}
+          <div>{randomCodeString}</div>
         </div>
       </div>
-      <div className="box-container p-4 sm:p-8 backdrop-blur-md">
-        <h2 className="header">ChatGPT</h2>
-        <div className="content-box">
-          <Form
-            aria-disabled={isSubmitting}
-            method="post"
-            ref={formRef}
-            onSubmit={handleFormSubmit}
-            replace
-          >
+      <div className="box-container p-4 sm:p-8 backdrop-blur-md border border-black flex flex-col">
+        <h2 className="header font-bold text-lg">User Input</h2> {/* Made the header text bold and slightly bigger */}
+        <div className="content-box flex-grow" style={{ wordWrap: 'break-word', overflow: 'auto' }}> {/* Added wordWrap and overflow styles to prevent horizontal overflow */}
+          <div>{chatHistory.map(chat => `${chat.role}: ${chat.content}\n`)}</div>
+        </div>
+        <Form
+          aria-disabled={isSubmitting}
+          method="post"
+          ref={formRef}
+          onSubmit={handleFormSubmit}
+          replace
+          className="w-full flex flex-col justify-between" // Added flex and justify-between to position the button at the bottom
+        >
+          <div className="flex justify-between items-end"> {/* Added a div to wrap the textarea and button */}
             <textarea
               id="message"
               aria-disabled={isSubmitting}
               ref={inputRef}
-              className="input-box"
-              placeholder="Ask a question"
+              className="input-box flex-grow mr-2" // Added flex-grow to make the textarea take up the remaining space and margin-right for spacing
+              placeholder="Type your message to ChatGPT here..."
               name="message"
               required
               rows={1}
               onKeyDown={submitFormOnEnter}
               minLength={2}
               disabled={isSubmitting}
-            />
-            <input
-              type="hidden"
-              value={JSON.stringify(chatHistory)} name="chat-history"
+              style={{ overflow: 'auto', resize: 'none', wordWrap: 'break-word' }} // Added wordWrap and overflow styles to prevent horizontal overflow and prevents user from manually resizing the textarea
+              onInput={e => { // Dynamically adjust the height of the textarea
+                const target = e.target as HTMLTextAreaElement;
+                target.style.height = 'auto';
+                target.style.height = `${target.scrollHeight}px`;
+              }}
             />
             <button
               aria-label="Submit"
@@ -147,15 +181,20 @@ export default function IndexPage() {
             >
               <SendIcon />
             </button>
-          </Form>
-          
-        </div>
+          </div>
+          <input
+            type="hidden"
+            value={JSON.stringify(chatHistory)} name="chat-history"
+          />
+        </Form>
       </div>
-      <div className="box-container p-4 sm:p-8 backdrop-blur-md">
-        <h2 className="header">Edited Code</h2>
-        <div className="content-box">
-          {/* Edited code goes here */}
+      <div className="box-container p-4 sm:p-8 backdrop-blur-md border border-black">
+        <h2 className="header font-bold text-lg">GPT Response</h2> {/* Made the header text bold and slightly bigger */}
+        <div className="content-box" style={{ wordWrap: 'break-word', overflow: 'auto' }}> {/* Added wordWrap and overflow styles to prevent horizontal overflow */}
+          <div>{gptResponse}</div> {/* Display the GPT response */}
         </div>
       </div>
     </main>
-  )};
+  )
+};
+
